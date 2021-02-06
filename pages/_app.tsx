cdf1f02fdbcd04973b012e2da73713dba089bd39
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2021-02-06 10:15:06 +0000

    Setup TailwindCSS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type { AppProps } from "next/app";
 
+import "tailwindcss/tailwind.css";
+
 function App({ Component, pageProps }: AppProps): JSX.Element {
   return <Component {...pageProps} />;
 }
