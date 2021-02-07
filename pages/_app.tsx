f523eb675b7c3b9a78c2315ba210f1507802fe2f
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2021-02-07 12:09:32 +0000

    Add Fathom for analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "nauth0";
 import { deserialize } from "superjson";
-
-import "tailwindcss/tailwind.css";
+import * as Fathom from "fathom-client";
 import { initSentry } from "lib/sentry";
 import { SessionProps } from "lib/session";
+import { useRouter } from "next/dist/client/router";
+
+import "tailwindcss/tailwind.css";
 
 initSentry();
 
 function App({ Component, pageProps }: AppProps): JSX.Element {
+  const router = useRouter();
+
+  useEffect(() => {
+    Fathom.load("LXSEWIWP", {
+      includedDomains: ["achievements.jamiedavenport.dev"],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on("routeChangeComplete", onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off("routeChangeComplete", onRouteChangeComplete);
+    };
+  }, []);
+
   let session;
   try {
     const props = deserialize<SessionProps>(pageProps);
